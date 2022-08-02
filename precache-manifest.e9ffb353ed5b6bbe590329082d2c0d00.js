a62712f6814d6a7a689827f37117e6bfe62c362c
Abdur Rafay <sheikhrafay5559@gmail.com>
2022-08-02 21:34:54 +0500

    Updates

---
+++ precache-manifest.e9ffb353ed5b6bbe590329082d2c0d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d4ece8099ba47ca8728",
+    "url": "/TextUtils-React.js-app/static/css/main.104ec82e.chunk.css"
+  },
+  {
+    "revision": "8d4ece8099ba47ca8728",
+    "url": "/TextUtils-React.js-app/static/js/main.8d4ece80.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React.js-app/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "234aebbd60285a69ff3c",
+    "url": "/TextUtils-React.js-app/static/js/2.234aebbd.chunk.js"
+  },
+  {
+    "revision": "2c283e2e8202caa492b3",
+    "url": "/TextUtils-React.js-app/static/js/runtime~main.2c283e2e.js"
+  },
+  {
+    "revision": "95e3de92c7602af03c47395c70e00934",
+    "url": "/TextUtils-React.js-app/index.html"
+  }
+];
